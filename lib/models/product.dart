edef73bd019b677ea4489daee88e66f91a57a473
Juri Chau <irujchaudhry@gmail.com>
2025-10-19 15:09:18 +0500

    quiz part added

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product{
+  final String name;
+  final String description;
+  final String step;
+  final String image;
+
+  Product({
+    required this.name,
+    required this.description,
+    required this.step,
+    required this.image,
+  });
+}
